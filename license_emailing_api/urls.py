6ab12334fa037b1cfd668dc91e895850ba97348d
Max <maxi635@hotmail.com>
2021-02-10 09:39:13 -0300

    Configuração swagger

--- license_emailing_api/urls.py
+++ license_emailing_api/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path, include
 from . import views
 from rest_framework import routers
-
+from rest_framework_swagger.views import get_swagger_view
 from license_emailing_api.views import LicensesView, LicenseDueDateAPIView
+
+
+schema_view = get_swagger_view(title='API Documentation License Emailing')
 
 router = routers.DefaultRouter()
 router.register('licenses', views.LicensesView, basename='licenses_list')
@@ -10,6 +13,7 @@ router.register('due-date', views.LicenseDueDateAPIView, basename='duedate_list'
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('api-docs/', schema_view),
 ]
 
 urlpatterns += router.urls
