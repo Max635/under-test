b5e0a3d250bb5a84ca172129211b91c259d46aae
Max <maxi635@hotmail.com>
2021-02-09 22:33:50 -0300

    Configuração views.py

--- license_emailing_api/views.py
+++ license_emailing_api/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.views.generic import ListView, CreateView
+from django.urls import reverse_lazy
+
+from rest_framework import viewsets, generics
+from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework.response import Response
+
+from .models import CustomerLicenseModel
+from .serializers import LicensesSerializer
+from .forms import LicenseForm
+
+
+class LicensesView(viewsets.ModelViewSet):
+    queryset = CustomerLicenseModel.objects.all()
+    serializer_class = LicensesSerializer
+
+class LicenseDueDateAPIView(viewsets.ReadOnlyModelViewSet):
+    queryset = CustomerLicenseModel.objects.all()
+    serializer_class = LicensesSerializer
+    filter_backends = (SearchFilter, OrderingFilter,)
+    search_fields = ['license_due_date',]
+
+class LicenseListView(ListView):
+    model = CustomerLicenseModel
+    context_object_name = 'license'
+
+class LicenseCreateView(CreateView):
+    model = CustomerLicenseModel
+    fields = ['name', 'email', 'package_name', 'license_type']
+    success_url = reverse_lazy('license_list')
